b1723dbca54e3ae2e2aba0134fff301a76b757ad
vladislav <vladislav.051094@yandex.ru>
2026-02-25 17:16:23 +0300

    added: svgr

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
     reactCompiler: true,
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule: { test: { test: (arg0: string) => unknown } }) =>
+            rule.test?.test?.(".svg")
+        );
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: {
+                    loader: "@svgr/webpack",
+                    options: {
+                        svgoConfig: {
+                            plugins: [
+                                {
+                                    name: "preset-default",
+                                    params: {
+                                        overrides: {
+                                            removeViewBox: false,
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            }
+        );
+
+        fileLoaderRule.exclude = /\.svg$/i;
+
+        return config;
+    },
 };
 
 export default nextConfig;
