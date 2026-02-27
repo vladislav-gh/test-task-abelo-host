5dd43449ae0752eb7f77556ae53d485030256fa2
vladislav <vladislav.051094@yandex.ru>
2026-02-27 17:30:08 +0300

    added: icon component

--- src/components/ui/icon/Icon.tsx
+++ src/components/ui/icon/Icon.tsx
@@ -1,3 +1,42 @@
-export function Icon() {
-    return <div></div>;
+import type { VariantProps } from "class-variance-authority";
+import type { ElProps } from "@/types";
+import type { IconKeys } from "./collection";
+
+import { cva } from "class-variance-authority";
+import clsx from "clsx";
+
+import { IconsCollection } from "./collection";
+import styles from "./styles.module.scss";
+
+type VariantsProps = VariantProps<typeof variants>;
+
+const variants = cva(styles.icon, {
+    variants: {
+        size: {
+            xs: styles["icon_size-xs"],
+            sm: styles["icon_size-sm"],
+            md: styles["icon_size-md"],
+            lg: styles["icon_size-lg"],
+            xl: styles["icon_size-xl"],
+        },
+    },
+    defaultVariants: {
+        size: "md",
+    },
+});
+
+const variantsClassName = (props: VariantsProps) => variants(props);
+
+export interface IconProps extends ElProps<"svg">, VariantsProps {
+    k: IconKeys;
+}
+
+export function Icon({ className, k, ...restProps }: IconProps) {
+    const Component = IconsCollection[k];
+
+    if (!Component) {
+        return;
+    }
+
+    return <Component className={clsx(variantsClassName(restProps), className)} {...restProps} />;
 }
---
+++ src/components/ui/icon/collection.ts
@@ -0,0 +1,21 @@
+import Cart from "./images/cart.svg";
+import CheckRounded from "./images/check-rounded.svg";
+import Envelope from "./images/envelope.svg";
+import Error404 from "./images/error-404.svg";
+import Error from "./images/error.svg";
+import LogoAbeloHost from "./images/logo-abelo-host.svg";
+import SignIn from "./images/sign-in.svg";
+import User from "./images/user.svg";
+
+export const IconsCollection = {
+    cart: Cart,
+    checkRounded: CheckRounded,
+    envelope: Envelope,
+    error404: Error404,
+    error: Error,
+    logoAbeloHost: LogoAbeloHost,
+    signIn: SignIn,
+    user: User,
+} as const;
+
+export type IconKeys = keyof typeof IconsCollection;
