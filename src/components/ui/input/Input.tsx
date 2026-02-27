a5db7cd56ebd4a7af73d6d11446ced422db2d665
vladislav <vladislav.051094@yandex.ru>
2026-02-27 23:22:09 +0300

    added: input component

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,3 +1,28 @@
-export function Input() {
-    return <input type="text" />;
+import type { ElProps } from "@/types";
+
+import clsx from "clsx";
+
+import styles from "./styles.module.scss";
+
+export interface InputProps extends ElProps<"input"> {
+    label?: string;
+    error?: string;
+}
+
+export function Input({ className, type = "text", label, error, ...restProps }: InputProps) {
+    return (
+        <label className={clsx(styles.container, className)}>
+            {label && <span className={styles.label}>{label}</span>}
+
+            <input
+                className={clsx(styles.input, {
+                    [styles.input_error]: error,
+                })}
+                type={type}
+                {...restProps}
+            />
+
+            {error && <span className={styles.error}>{error}</span>}
+        </label>
+    );
 }
