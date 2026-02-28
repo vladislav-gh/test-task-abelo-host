7073e1bd907f7f6748c6f3088b2eafd7ea35a193
vladislav <vladislav.051094@yandex.ru>
2026-02-28 14:30:50 +0300

    added: page login

--- src/components/pages/login/Page.tsx
+++ src/components/pages/login/Page.tsx
@@ -1,5 +1,21 @@
+import { FormSignIn } from "@/components/auth";
+import { Text } from "@/components/ui";
+
 import { PageLayout } from "../Layout";
+import styles from "./styles.module.scss";
 
 export function PageLogin() {
-    return <PageLayout>Login page</PageLayout>;
+    return (
+        <PageLayout className={styles.page}>
+            <div className={styles.info}>
+                <Text as="h1" variant="title">
+                    Login
+                </Text>
+
+                <p className={styles.description}>Enter your credentials to access your account</p>
+            </div>
+
+            <FormSignIn className={styles.form} />
+        </PageLayout>
+    );
 }
