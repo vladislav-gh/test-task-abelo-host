37898ee3ffaab7199e0e6792e9c23963d527ac27
vladislav <vladislav.051094@yandex.ru>
2026-02-28 03:37:56 +0300

    added: page 404

--- src/components/pages/not-found/Page.tsx
+++ src/components/pages/not-found/Page.tsx
@@ -1,5 +1,23 @@
+import { Button, Icon, Text } from "@/components/ui";
+import { PAGES } from "@/config";
+
 import { PageLayout } from "../Layout";
+import styles from "./styles.module.scss";
 
 export function Page404() {
-    return <PageLayout>404 page</PageLayout>;
+    return (
+        <PageLayout className={styles.page}>
+            <Icon className={styles.icon} k="error404" />
+
+            <Text as="h1" variant="subtitle">
+                Oops! This Page is Not Found.
+            </Text>
+
+            <p className={styles.text}>The requested page dose not exist</p>
+
+            <Button className={styles.button} href={PAGES.home}>
+                Go Home
+            </Button>
+        </PageLayout>
+    );
 }
