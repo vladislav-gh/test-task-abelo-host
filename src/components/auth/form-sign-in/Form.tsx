348fc9f950e997c823717af6c95651de7fdb6bae
vladislav <vladislav.051094@yandex.ru>
2026-02-28 14:30:40 +0300

    added: auth form sign in view

--- src/components/auth/form-sign-in/Form.tsx
+++ src/components/auth/form-sign-in/Form.tsx
@@ -1,3 +1,36 @@
-export function FormSignIn() {
-    return <form></form>;
+import type { ElProps } from "@/types";
+
+import clsx from "clsx";
+
+import { Button, Input, Text } from "@/components/ui";
+
+import styles from "./styles.module.scss";
+
+export type FormSignInProps = ElProps<"form">;
+
+export function FormSignIn({ className, ...restProps }: FormSignInProps) {
+    return (
+        <form className={clsx(styles.form, className)} {...restProps}>
+            <div className={styles.fields}>
+                <Input name="username" label="Username*" placeholder="Enter your name" minLength={3} required />
+
+                <Input
+                    type="password"
+                    name="password"
+                    label="Password*"
+                    placeholder="Enter your password"
+                    minLength={3}
+                    required
+                />
+            </div>
+
+            <Button type="submit" block>
+                Login
+            </Button>
+
+            <Text className={styles.error} variant="caption">
+                Error message
+            </Text>
+        </form>
+    );
 }
